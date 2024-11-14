f8a837b25d063b1c9ef6d664677a1f0b9313deac
Carlos Davi <carlosdavi004@gmail.com>
2024-11-13 22:22:23 -0300

    Feat: Criado logica de atualização de departamento

--- app/Rules/DepartmentRule.php
+++ app/Rules/DepartmentRule.php
@@ -29,6 +29,32 @@ class DepartmentRule
         return true;
     }
 
+    public function update($request)
+    {
+        $rules = [
+            'id' => 'required|string|max:100',
+            'name' => 'required|string|min:3|max:50',
+        ];
+
+        $messages = [
+            'id.required' => 'O ID do departamento é obrigatório',
+            'id.string' => 'O ID do departamento deve ser uma string',
+            'id.max' => 'O ID do departamento não pode ter mais de 100 caracteres',
+            'name.required' => 'O nome do departamento é obrigatório',
+            'name.string' => 'O nome deve ser uma string',
+            'name.min' => 'O nome do departamento não pode ter menos de 3 caracteres',
+            'name.max' => 'O nome do departamento não pode ter mais de 50 caracteres',
+        ];
+
+        $validator = Validator::make($request->all(), $rules, $messages);
+
+        if ($validator->fails()) {
+            throw new ValidationException($validator);
+        }
+
+        return true;
+    }
+
     public function delete($id)
     {
         $rules = [
@@ -50,32 +76,4 @@ class DepartmentRule
         return true;
     }
 
-    // public function update($request)
-    // {
-    //     $rules = [
-    //         'id' => 'required|string|max:100',
-    //         'name' => 'required|string|min:3|max:30',
-    //         'type' => 'required|string',
-    //     ];
-
-    //     $messages = [
-    //         'id.required' => 'O ID da categoria é obrigatório',
-    //         'id.string' => 'O ID da categoria deve ser uma string',
-    //         'id.max' => 'O ID da categoria não pode ter mais de 100 caracteres',
-    //         'name.required' => 'O nome da categoria é obrigatório',
-    //         'name.string' => 'O nome deve ser uma string',
-    //         'name.min' => 'O nome da categoria não pode ter menos de 3 caracteres',
-    //         'name.max' => 'O nome da categoria não pode ter mais de 30 caracteres',
-    //         'type.required' => 'O tipo de movimentação da categoria é obrigatório',
-    //         'type.string' => 'O tipo de movimentação deve ser uma string',
-    //     ];
-
-    //     $validator = Validator::make($request->all(), $rules, $messages);
-
-    //     if ($validator->fails()) {
-    //         throw new ValidationException($validator);
-    //     }
-
-    //     return true;
-    // }
 }
