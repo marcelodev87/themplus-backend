9691df2b0476927e3a3387c5a3511c8df81abfe5
Carlos Davi <carlosdavi004@gmail.com>
2025-01-09 18:11:41 -0300

    Feat: Retirado regra de unico para email,cpf e cnpj

---
+++ database/migrations/2025_01_09_210857_remove_unique_from_enterprises_table.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    public function up()
+    {
+        Schema::table('enterprises', function (Blueprint $table) {
+            $table->dropUnique(['cpf']);
+            $table->dropUnique(['cnpj']);
+            $table->dropUnique(['email']);
+        });
+    }
+
+    public function down()
+    {
+        Schema::table('enterprises', function (Blueprint $table) {
+            $table->unique('cpf');
+            $table->unique('cnpj');
+            $table->unique('email');
+        });
+    }
+};
