38e7a0c307f0de66532e795f047244407bdfb0aa
ednardo gomes <ednardogomesdev@gmail.com>
2025-06-10 17:11:53 -0300

    feat: create table feedback saved

---
+++ database/migrations/2025_06_06_194119_create_table_feedback_saved.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+
+    public function up(): void
+    {
+        Schema::create('feedbacks_saved', function(Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->string('user_name');
+            $table->string('user_email');
+            $table->string('enterprise_name');
+            $table->text('message', 5000);
+            $table->string('date_feedback');
+            $table->timestamps();
+        });
+    }
+
+
+    public function down(): void
+    {
+        Schema::dropIfExists('feedbacks_saved');
+    }
+};
+
