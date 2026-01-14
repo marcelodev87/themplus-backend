f65af399ff0faf7f2e1d2615b4b684c6254549a7
For-Davi <carlosdavi004@gmail.com>
2026-01-14 13:23:39 -0300

    fix: alteração em coluna

---
+++ database/migrations/2026_01_14_161925_alter_column_in_payments_themplus.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+
+    public function up(): void
+    {
+        Schema::table('payments_themplus', function (Blueprint $table) {
+            $table->decimal('fine_value', 10, 2)->nullable()->change();
+            $table->string('fine_type')->nullable()->change();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::table('payments_themplus', function (Blueprint $table) {
+            $table->dropColumn('fine_value')->nullable(false)->change();
+            $table->dropColumn('fine_type')->nullable(false)->change();
+        });
+    }
+};
